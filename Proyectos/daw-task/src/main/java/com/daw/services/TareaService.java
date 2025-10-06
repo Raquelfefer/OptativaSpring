9a0ae6b8a24ba240ccf89b2943a915f7c016f210
RaquelFerFer <rferfer600@g.educaand.es>
2025-10-06 12:46:50 +0200

    avance

--- Proyectos/daw-task/src/main/java/com/daw/services/TareaService.java
+++ Proyectos/daw-task/src/main/java/com/daw/services/TareaService.java
@@ -1,12 +1,15 @@
 package com.daw.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.daw.persistence.entities.Estado;
 import com.daw.persistence.entities.Tarea;
 import com.daw.persistence.repositories.TareaRepository;
+import com.daw.services.exceptions.TareaNotFoundException;
 
 @Service
 public class TareaService {
@@ -14,10 +17,12 @@ public class TareaService {
 	@Autowired
 	private TareaRepository tareaRepository;
 	
+	//findAll
 	public List<Tarea> findAll(){
 		return this.tareaRepository.findAll();
 	}
 	
+	//findById
 	public Tarea findById(int idTarea) {
 		if(!this.tareaRepository.existsById(idTarea)) {
 			throw new TareaNotFoundException("No existe la tarea con ID: " + idTarea);
@@ -25,4 +30,21 @@ public class TareaService {
 		
 		return this.tareaRepository.findById(idTarea).get();
 	}
+	
+	//create
+	public Tarea create(Tarea tarea) {
+		if(tarea.getFechaVencimiento().isBefore(LocalDate.now())) {
+			throw new TareaException()
+		}
+		
+		tarea.setId(0);
+		tarea.setEstado(Estado.PENDIENTE);
+		tarea.setFechaCreacion(LocalDate.now());
+		
+		return this.tareaRepository.save(tarea);
+	}
+	
+	
+	
+	//update
 }
--- Proyectos/daw-task/src/main/java/com/daw/web/controllers/TareaController.java
+++ Proyectos/daw-task/src/main/java/com/daw/web/controllers/TareaController.java
@@ -4,13 +4,18 @@ package com.daw.web.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daw.persistence.entities.Tarea;
 import com.daw.services.TareaService;
+import com.daw.services.exceptions.TareaNotFoundException;
 
 
 @RestController
@@ -25,9 +30,20 @@ public class TareaController {
 		return this.tareaService.findAll();
 	}
 	
-	@GetMapping("/("idTarea)")
-	public Tarea findById(@PathVariable int idTarea) {
-		return this.tareaService.findById(idTarea);
+	@GetMapping("/{idTarea}")
+	public ResponseEntity<?> findById(@PathVariable int idTarea) {
+		try {
+			return ResponseEntity.ok(this.tareaService.findById(idTarea));
+		}
+		catch(TareaNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody Tarea tarea){
+		
 	}
 	
 }
